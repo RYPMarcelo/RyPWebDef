e70347b09b1669744fbcb0750d55275f7e8b9842
RYPMarcelo <bypizarro.mpl@gmail.com>
2024-08-15 00:45:57 -0400

    pdf

---
+++ JS/script-pdf.js
@@ -0,0 +1,8 @@
+let ropaclick = Document.getelementbyid(ropa);  
+let eppclick = Document.getelementbyid(epp);  
+let zapatoclick = Document.getelementbyid(zapato);  
+let imgchange = Document.getelementbyid(zapato);  
+
+ropaclick.onclick = funcion() {
+     imgchange.src = "../img-prod-destacados/ROPA-CORPORATIVA-modificada.pdf type='aplication/pdf' width='420px' height='630'";        
+}
